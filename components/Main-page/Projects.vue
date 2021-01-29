26f89bfc0839d7092cb8274461529b99bf0cf518
Vlad613 <vladislav613zharnikov@gmail.com>
2021-01-29 22:36:57 +0300

    main-page-projects-bag-fix

--- components/Main-page/Projects.vue
+++ components/Main-page/Projects.vue
@@ -4,8 +4,8 @@
     <div class="content-block">
       <div class="content-img-wrap"
            v-for="(item, index) in contentImgs"
-           key="index"
-           :style="'grid-area:'+(index+1)+' '+(index+1)">
+           :key="index"
+           :style="'grid-area:'+(projectsGridAreas[index])">
         <Image_Scale_Block :img="item.img"/>
         <div class="item-project-title">
           {{item.title}}
@@ -33,10 +33,11 @@
         data() {
             return {
                 contentImgs: [
-                    {img:img1, title:'RB'},
-                    {img:img2, title:'Nyenrode'},
-                    {img:img3, title:"5CA int."},
-                    {img:img4, title:'OG'}]
+                    {img: img1, title: 'RB'},
+                    {img: img2, title: 'Nyenrode'},
+                    {img: img3, title: "5CA int."},
+                    {img: img4, title: 'OG'}],
+                projectsGridAreas: ['a', 'b', 'c', 'd']
             }
         }
     }
@@ -54,6 +55,7 @@
       display: grid;
       grid-template-rows: 840px 840px;
       grid-template-columns: calc((100% - 60px) / 2) calc((100% - 60px) / 2);
+      grid-template-areas: "a b" "c d";
       gap: 60px;
 
       .content-img-wrap {
