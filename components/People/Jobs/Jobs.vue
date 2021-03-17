8401dcfe2c771cfb2321809bc20ec9f200be650a
migi <wenmigi.yt@gmail.com>
2021-03-17 19:08:14 +0300

    fix

--- components/People/Jobs/Jobs.vue
+++ components/People/Jobs/Jobs.vue
@@ -9,26 +9,7 @@
     <!--      @clicked="$emit('clicked')"-->
     <!--    />-->
     <p class="team__text_p content-p">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur esse,
-      est harum inventore mollitia odit officiis quis tenetur vel voluptatum.
-      Dolor explicabo id magnam nobis ullam! Ab ad amet at autem, beatae dicta
-      dignissimos dolorem doloribus enim eos et eveniet fugiat harum ipsum iste
-      laborum libero magni minus neque nesciunt optio quia quis ratione repellat
-      sint tempora tenetur voluptatem. Adipisci alias aut consequuntur corporis
-      deleniti exercitationem iure, molestias, mollitia non nostrum nulla
-      possimus quasi reiciendis sequi veniam veritatis voluptatibus? Alias
-      commodi distinctio eius id incidunt ipsam laborum minus obcaecati odit
-      repellat. Accusamus ad alias asperiores at delectus dolore dolores ducimus
-      earum enim ex excepturi expedita harum inventore iure iusto magni
-      quibusdam quidem quo saepe sit sunt tempora tempore tenetur, totam velit
-      veniam voluptatibus? A ab accusamus accusantium aliquam animi blanditiis
-      commodi corporis cumque debitis est ex excepturi facere fuga iusto minus
-      neque non odio pariatur possimus quas, quidem reprehenderit similique
-      tempore temporibus ullam vitae, voluptates? Ab aliquid animi asperiores
-      assumenda, at commodi consectetur debitis dolore ea eos excepturi facere
-      fugit impedit ipsa iusto minima minus neque, obcaecati quasi quod repellat
-      similique sint sit unde ut vero vitae? Aperiam, commodi corporis deserunt
-      dicta labore nobis perspiciatis possimus quia ratione vero voluptate.
+      {{ data.description }}
     </p>
   </div>
 </template>
